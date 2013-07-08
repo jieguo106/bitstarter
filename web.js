16a37effe717f77db2a88ca25dd9461220fae3fc
Jie Guo <jieguo@ubuntu.(none)>
2013-07-07 19:12:31 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-
+var fs = require('fs');
+var buf = fs.readFilesSync('index.html');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
